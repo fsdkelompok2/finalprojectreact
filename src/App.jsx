a257b30e443145abe5a46036d55125049d16ccba
Ali Akbar <maliakbar879@gmail.com>
2024-02-20 20:39:51 +0700

    commit pertama

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-9xl">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        incidunt veniam vitae exercitationem. Reprehenderit, nemo distinctio.
+        Totam atque minima recusandae magnam provident consequuntur id
+        architecto animi vero harum? Saepe, eum.
+      </h1>
+    </>
+  );
+}
+
+export default App;
